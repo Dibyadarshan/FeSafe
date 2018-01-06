e20b6f27440beab908fff9f27e2b1403718f8f0b
dibyadarshan <dibyadarshan.hota@gmail.com>
2018-01-07 01:09:22 +0530

    Added cover page

---
+++ Desktop/FeSafe/app/src/main/java/com/example/dibyadarshan/fesafe/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dibyadarshan.fesafe;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int PERMISSION_REQUEST_CODE = 1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_DENIED) {
+
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {Manifest.permission.SEND_SMS};
+
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+
+            }
+        }
+    }
+
+    public void start(View view) {
+        Intent intent = new Intent(getApplicationContext(), second.class);
+        startActivity(intent);
+    }
+}
